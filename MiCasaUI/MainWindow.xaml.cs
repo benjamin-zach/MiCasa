a884e056291596de14138cf58361f0b6cf9b7830
Benjamin Zach <benjamin.zach@freenet.de>
2019-01-05 13:04:10 +0100

    Initial commit

---
+++ MiCasaUI/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiCasaUI
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            addRecepyButton.Click += OnAddRecepyButtonClicked;
+            removeRecepyButton.Click += OnRemoveRecepyButtonClicked;
+            refreshRecepiesButton.Click += OnRefreshRecepiesButtonClicked;
+        }
+
+        private void OnAddRecepyButtonClicked(object sender, RoutedEventArgs e)
+        {
+            AddRecepyWindow addRecepyWindow = new AddRecepyWindow();
+            addRecepyWindow.Show();
+        }
+
+        private void OnRemoveRecepyButtonClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnRefreshRecepiesButtonClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
